334528173f4162989e0b53be05fac7f9b26c5d53
crgonzalezvi <crgonzalezvi@unal.edu.co>
2025-02-17 15:49:25 -0500

    Clase de Rutas de React

--- react-first-project/src/App.jsx
+++ react-first-project/src/App.jsx
@@ -2,33 +2,35 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import PrimerComponente from './PrimerComponente.jsx'
+import CardComponente from './CardComponente.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+   <Router>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+                <Link to="/tabla" className="nav-link">Tabla</Link>
+                <Link to="/card" className="nav-link">Card</Link>
+            </ul>
+          </div>
+        </nav>
+
+        <Routes>
+          <Route path="/tabla" element={<PrimerComponente />} />
+        </Routes>
+        <Routes>
+          <Route path="/card" element={<CardComponente />} />
+        </Routes>
+        </div>
+    </Router>   
   )
 }
 
